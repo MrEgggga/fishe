6e94fe0230223786b83217421b2eda885a37c971
jmibo <68486196+MrEgggga@users.noreply.github.com>
2024-03-02 14:32:10 -0800

    delete everything (bad idea)

--- source/main.lua
+++ source/main.lua
@@ -1,87 +1,43 @@
--- Name this file `main.lua`. Your game can use multiple source files if you wish
--- (use the `import "myFilename"` command), but the simplest games can be written
--- with just `main.lua`.
-
--- You'll want to import these in just about every project you'll work on.
-
 import "CoreLibs/object"
 import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/crank"
 
--- Declaring this "gfx" shorthand will make your life easier. Instead of having
--- to preface all graphics calls with "playdate.graphics", just use "gfx."
--- Performance will be slightly enhanced, too.
--- NOTE: Because it's local, you'll have to do it in every .lua source file.
 
 local gfx <const> = playdate.graphics
+local bar = {
+    height = 200,
+    pos = 80,
+    vel = 0,
+    areaHeight = 40,
+    fishPos = 100,
+    fishVel = 40,
+    gravity = 0.5,
+    accel = 1.2,
+    getAccel = function ()
+        if playdate.buttonIsPressed(playdate.kButtonUp) then
+            return 1
+        end
+        return 0
+    end
+}
 
--- Here's our player sprite declaration. We'll scope it to this file because
--- several functions need to access it.
-
-local playerSprite = nil
 local yvel = 0
 local fp = playdate.sound.fileplayer.new()
 fp:load("sounds/S2A Sky Chase")
 fp:play(0)
 
--- A function to set up our game environment.
 
-function myGameSetUp()
+local barSlice = gfx.nineSlice.new("images/bar0.png", 3, 3, 3, 3)
+local controllableSlice = gfx.nineSlice.new("images/bar1.png", 3, 3, 3, 3)
+local barWidth<const> = 24
 
-    -- Set up the player sprite.
-    -- The :setCenter() call specifies that the sprite will be anchored at its center.
-    -- The :moveTo() call moves our sprite to the center of the display.
-
-    local playerImage = gfx.image.new("Images/playerImage")
-    assert( playerImage ) -- make sure the image was where we thought
-
-	local fishMeterImg = gfx.image.new("Images/FishingMeter")
-    assert( fishMeterImg )
-
-	FishMeterSprite = gfx.sprite.new ( fishMeterImg )
-	FishMeterSprite:moveTo(200,120)
-	FishMeterSprite:add()
-    playerSprite = gfx.sprite.new( playerImage )
-    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
-    playerSprite:add() -- This is critical!
-
-    -- We want an environment displayed behind our sprite.
-    -- There are generally two ways to do this:
-    -- 1) Use setBackgroundDrawingCallback() to draw a background image. (This is what we're doing below.)
-    -- 2) Use a tilemap, assign it to a sprite with sprite:setTilemap(tilemap),
-    --       and call :setZIndex() with some low number so the background stays behind
-    --       your other sprites.
-
-    local backgroundImage = gfx.image.new( "Images/background" )
-    assert( backgroundImage )
-
-    gfx.sprite.setBackgroundDrawingCallback(
-        function( x, y, width, height )
-            -- x,y,width,height is the updated area in sprite-local coordinates
-            -- The clip rect is already set to this area, so we don't need to set it ourselves
-            backgroundImage:draw( 0, 0 )
-        end
-    )
-
-end
 
 myGameSetUp()
 
 function playdate.update()
 
-    if playdate.buttonIsPressed( playdate.kButtonUp ) then
-        yvel-=1.2
-    end
 
-	yvel += 0.5
-
-	playerSprite:moveBy( 0, yvel )
-    
-	--fp:setRate()
-
-    gfx.sprite.update()
-    playdate.timer.updateTimers()
 
 end
