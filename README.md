f5f9d31f5edf0bcb4a95af1925ab31e0f6e3261b
jmibo <68486196+MrEgggga@users.noreply.github.com>
2024-03-02 12:52:26 -0800

    various

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fishe
 ing
+/fiʃ/
--- source/main.lua
+++ source/main.lua
@@ -21,7 +21,6 @@ local gfx <const> = playdate.graphics
 -- several functions need to access it.
 
 local playerSprite = nil
-local xvel = 0
 local yvel = 0
 local fp = playdate.sound.fileplayer.new()
 fp:load("sounds/S2A Sky Chase")
@@ -41,12 +40,12 @@ function myGameSetUp()
 	local fishMeterImg = gfx.image.new("Images/FishingMeter")
     assert( fishMeterImg )
 
-    playerSprite = gfx.sprite.new( playerImage )
-    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
-    playerSprite:add() -- This is critical!
 	FishMeterSprite = gfx.sprite.new ( fishMeterImg )
 	FishMeterSprite:moveTo(200,120)
 	FishMeterSprite:add()
+    playerSprite = gfx.sprite.new( playerImage )
+    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
+    playerSprite:add() -- This is critical!
 
     -- We want an environment displayed behind our sprite.
     -- There are generally two ways to do this:
@@ -68,44 +67,19 @@ function myGameSetUp()
 
 end
 
--- Now we'll call the function above to configure our game.
--- After this runs (it just runs once), nearly everything will be
--- controlled by the OS calling `playdate.update()` 30 times a second.
-
 myGameSetUp()
-
--- `playdate.update()` is the heart of every Playdate game.
--- This function is called right before every frame is drawn onscreen.
--- Use this function to poll input, run game logic, and move sprites.
 
 function playdate.update()
 
-    -- Poll the d-pad and move our player accordingly.
-    -- (There are multiple ways to read the d-pad; this is the simplest.)
-    -- Note that it is possible for more than one of these directions
-    -- to be pressed at once, if the user is pressing diagonally.
-
     if playdate.buttonIsPressed( playdate.kButtonUp ) then
-        yvel-=2
+        yvel-=1.2
     end
-    if playdate.buttonIsPressed( playdate.kButtonRight ) then
-        xvel+=2
-    end
-    if playdate.buttonIsPressed( playdate.kButtonDown ) then
-        yvel+=2
-    end
-    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
-        xvel-=2
-    end
-	yvel -= playdate.getCrankTicks(10)
-	playerSprite:moveBy( xvel, yvel )
-	xvel*=0.8
-	yvel*=0.8
-	--fp:setRate()
 
-    -- Call the functions below in playdate.update() to draw sprites and keep
-    -- timers updated. (We aren't using timers in this example, but in most
-    -- average-complexity games, you will.)
+	yvel += 0.5
+
+	playerSprite:moveBy( 0, yvel )
+    
+	--fp:setRate()
 
     gfx.sprite.update()
     playdate.timer.updateTimers()
