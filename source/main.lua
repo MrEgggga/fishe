016740718878ee38aa1cd75c3c9e6a0cbf954d98
TaelsDaFoox <bazmulligan@gmail.com>
2024-03-02 15:44:32 -0800

    overworld lua draft
    minecraft

--- source/main.lua
+++ source/main.lua
@@ -5,6 +5,11 @@ import "CoreLibs/timer"
 import "CoreLibs/crank"
 local gfx <const> = playdate.graphics
 
+ScrollX = -200
+ScrollY = -120
+xvel = 0
+yvel = 0
+
 function GfxSetup()
     local mapImage = gfx.image.new("Images/map")
     assert( mapImage )
@@ -13,19 +18,33 @@ function GfxSetup()
     mapSprite:moveTo( 200, 120 )
     mapSprite:add()
 
-    local backgroundImage = gfx.image.new( "Images/background" )
-    assert( backgroundImage )
-
-    gfx.sprite.setBackgroundDrawingCallback(
-        function( x, y, width, height )
-            backgroundImage:draw( 0, 0 )
-        end
-    )
-
 end
 
-function playdate.update()
+GfxSetup()
 
+function playdate.update()
+    if playdate.buttonIsPressed( playdate.kButtonUp ) then
+        yvel-=2
+    end
+    if playdate.buttonIsPressed( playdate.kButtonRight ) then
+        xvel+=2
+    end
+    if playdate.buttonIsPressed( playdate.kButtonDown ) then
+        yvel+=2
+    end
+    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+        xvel-=2
+    end
+
+    ScrollX += xvel
+    ScrollY += yvel
+
+    xvel*=0.7
+	yvel*=0.7
+
+
+    mapSprite:moveTo( 0-ScrollX, 0-ScrollY )
+    gfx.setBackgroundColor(gfx.kColorBlack)
     gfx.sprite.update()
     playdate.timer.updateTimers()
 end
