7b9382ab6b7d92ed0e1645688652a342bc0cb6fe
jmibo <68486196+MrEgggga@users.noreply.github.com>
2024-03-03 11:52:32 -0800

    Summary (required)

--- source/lots_of_bars.lua
+++ source/lots_of_bars.lua
@@ -2,19 +2,21 @@ import "bars_thing"
 
 class("barsManager").extends()
 
-local keys <const> = {playdate.kButtonUp, playdate.kButtonB, playdate.kButtonA}
+local keys <const> = {playdate.kButtonUp, playdate.kButtonB, playdate.kButtonA, playdate.kButtonRight}
 
 barsManager.kIncomplete = 0
 barsManager.kSuccess = 2
 barsManager.kFailure = 1
 
-function barsManager:init()
+function barsManager:init(fish_caught, reduce_bar_drain, area_size)
     self.currentBars = {}
-    self.numBars = 3
+    self.numBars = math.min(math.ceil(fish_caught / 3), 4)
     self.added = 1
     self.complete = 0
     self.strikes = 0
     self.frameCounter = 0
+    self.areaSize = area_size
+    self.barDrain = 1 - reduce_bar_drain * 0.2
     self.frames = {math.random(0, 150)}
     for i=1,self.numBars-1 do
         table.insert(self.frames, self.frames[i] + math.random(0, 90))
@@ -42,7 +44,7 @@ function barsManager:update()
         return barsManager.kSuccess
     end
     if self.added <= self.numBars and self.frameCounter == self.frames[self.added] then
-        local bar = barThing()
+        local bar = barThing(self.barDrain, self.areaSize)
         table.insert(self.currentBars, bar)
         bar:moveTo(36 * self.added, 120)
         bar.key = keys[self.added]
