eba089cfd408f7dfeb185f08a5cd1f3a20294038
TaelsDaFoox <bazmulligan@gmail.com>
2024-03-02 18:53:07 -0800

    slightly functional player gfx

--- source/main.lua
+++ source/main.lua
@@ -3,6 +3,7 @@ import "CoreLibs/graphics"
 import "CoreLibs/sprites"
 import "CoreLibs/timer"
 import "CoreLibs/crank"
+import "CoreLibs/animation"
 local gfx <const> = playdate.graphics
 
 ScrollX = -200
@@ -13,10 +14,21 @@ yvel = 0
 function GfxSetup()
     local mapImage = gfx.image.new("Images/map")
     assert( mapImage )
+    local playerImgs = gfx.imagetable.new("Images/Player")
+    assert( playerImgs )
 
     mapSprite = gfx.sprite.new( mapImage )
     mapSprite:moveTo( 200, 120 )
     mapSprite:add()
+
+    walkD = gfx.animation.loop.new(1, playerImgs, true)
+    --set start and end frames for animation!!
+
+    --PlayerSprite = gfx.sprite.new( mapImage )
+    --PlayerSprite:moveTo( 64, 64 )
+    --PlayerSprite:add()
+
+    
 
 end
 
@@ -45,5 +57,6 @@ function playdate.update()
     mapSprite:moveTo( 0-math.floor(0.5+(ScrollX/2))*2, 0-math.floor(0.5+(ScrollY/2))*2 )
     gfx.setBackgroundColor(gfx.kColorBlack)
     gfx.sprite.update()
+    walkD:draw(160,80)
     playdate.timer.updateTimers()
 end
